c86aa558fc70adabc53f7b45ee1bda8f059c2be0
Danielvi <danielvilopez@gmail.com>
2025-06-03 19:45:04 -0400

    Descarga los paquetes necesarios, injecta el contexto en el Service y Configura el BlazoredToast

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
+using Blazored.Toast;
+using Microsoft.EntityFrameworkCore;
 using StockManager.Components;
+using StockManager.Dal;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazoredToast();
+
+var ConStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContextFactory<Contexto>(options =>
+    options.UseNpgsql(ConStr));
 
 var app = builder.Build();
 
