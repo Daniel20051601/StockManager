9786a6ed2de047605d386d1672bb2dea28edfde9
Danielvi <danielvilopez@gmail.com>
2025-06-03 19:41:52 -0400

    Crea las carpetas necesarias

---
+++ Dal/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using StockManager.Models;
+
+namespace StockManager.Dal
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        
+        public DbSet<Productos> Productos { get; set; }
+
+        public Contexto() { }
+    }
+}
---
+++ Models/Productos.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockManager.Models;
+
+public class Productos
+{
+    [Key]
+    public int ProductoId { get; set; }
+}
