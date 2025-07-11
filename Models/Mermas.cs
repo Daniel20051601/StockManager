fe28f5e2322992af4b23c7ed635ede1c2f6aa63f
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-07-10 21:20:52 -0600

    Componente mermas completado

---
+++ Models/Mermas.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace StockManager.Models;
+
+public class Mermas
+{
+
+    [Key]
+    public int MermaId { get; set; }
+
+    [Required]
+    public int ProductoId { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public Producto? Producto { get; set; } // Propiedad de navegación
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0.")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "La razón es requerida.")]
+    [StringLength(500, ErrorMessage = "La razón no puede exceder 500 caracteres.")]
+    public string? Razon { get; set; }
+
+    public DateTime FechaMerma { get; set; } = DateTime.UtcNow;
+}
