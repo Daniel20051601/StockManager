bd4a442e88d99637f7fbc8968bd4c383610a2902
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-31 14:42:37 -0400

    Mermas completado

---
+++ Migrations/20250725184843_Mermitas.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mermitas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<int>(
+                name: "Cantidad",
+                table: "Mermas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaMerma",
+                table: "Mermas",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductoId",
+                table: "Mermas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Razon",
+                table: "Mermas",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mermas_ProductoId",
+                table: "Mermas",
+                column: "ProductoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mermas_productos_ProductoId",
+                table: "Mermas",
+                column: "ProductoId",
+                principalTable: "productos",
+                principalColumn: "ProductoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mermas_productos_ProductoId",
+                table: "Mermas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Mermas_ProductoId",
+                table: "Mermas");
+
+            migrationBuilder.DropColumn(
+                name: "Cantidad",
+                table: "Mermas");
+
+            migrationBuilder.DropColumn(
+                name: "FechaMerma",
+                table: "Mermas");
+
+            migrationBuilder.DropColumn(
+                name: "ProductoId",
+                table: "Mermas");
+
+            migrationBuilder.DropColumn(
+                name: "Razon",
+                table: "Mermas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Ventas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ventas_UsuarioId",
+                table: "Ventas",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ventas_Usuarios_UsuarioId",
+                table: "Ventas",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "UsuarioId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
