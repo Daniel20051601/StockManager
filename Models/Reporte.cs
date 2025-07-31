5011f39617dfbc63a886b76f6910e4042f444691
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-31 10:22:09 -0400

    Migraciones y models

---
+++ Models/Reporte.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManager.Models;
+
+public class Reporte
+{
+    [Key]
+    public int ReporteId { get; set; }
+
+    [ForeignKey("Proveedor")]
+    public int ProveedorId { get; set; }
+
+    [Required, MaxLength(200)]
+    public string Asunto { get; set; } = null!;
+
+    [Required, MaxLength(1000)]
+    public string Descripcion { get; set; } = null!;
+
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+    // Navegación
+    public Proveedor Proveedor { get; set; } = null!;
+}
